8a1c01bf45ab8f937d359e12622b25749df93e02
root <root@linux-box.us-central1-a.c.master-coder-280116.internal>
2020-06-18 15:59:03 +0000

    Revert "flask-api pract"
    This reverts commit 4d5498fb80ef27e779047a1ed07393a9de8f17ee.

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-
-
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'shwetank'}
-
-class Home(Resource):
-    def get(self):
-        return {'shwetank': 'Restfull API'}
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(Home, '/home')
-
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')  
