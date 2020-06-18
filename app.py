4d5498fb80ef27e779047a1ed07393a9de8f17ee
root <root@linux-box.us-central1-a.c.master-coder-280116.internal>
2020-06-18 15:54:47 +0000

    flask-api pract

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'shwetank'}
+
+class Home(Resource):
+    def get(self):
+        return {'shwetank': 'Restfull API'}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Home, '/home')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  
+    app.run(debug=True, host='0.0.0.0')
